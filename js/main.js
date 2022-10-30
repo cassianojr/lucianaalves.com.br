ee975c061664ff13b2e9416fb0bc6786c3b54a3b
cassianojr <cassiano.jr.2009@hotmail.com>
2022-10-30 11:48:06 -0300

    some changes requested by customer

--- js/main.js
+++ js/main.js
@@ -298,3 +298,24 @@
 
 })(jQuery);
 
+const sections = document.querySelectorAll(".sections");
+const navLi = document.querySelectorAll(".nav-item");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id");
+
+	}
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
+
